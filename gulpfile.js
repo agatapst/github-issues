6f0d95e151ae2a5a3c709e75349ce8b3920942ab
Agata Stawarz <agata.stawarz94@gmail.com>
2019-07-05 16:19:17 +0200

    Add initial project structure

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var cssnano = require("gulp-cssnano");
+
+function compileScss() {
+    return (
+        gulp
+            .src("scss/main.scss")
+            .pipe(sass())
+            .pipe(cssnano())
+            .on("error", sass.logError)
+            .pipe(gulp.dest("css"))
+    );
+}
+
+function watch() {
+    compileScss();
+    gulp.watch('scss/*.scss', compileScss)
+}
+
+exports.watch = watch;
